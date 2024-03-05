45a379bcbd11e159d2e508a9f3892b466acdc4c8
xabibullayevuser <xusniddinxabibullayev25@gmail.com>
2024-03-05 23:05:08 +0500

    4-darsga uyga vazifa

---
+++ app.js
@@ -0,0 +1,31 @@
+// 10-masala
+
+// function findLongestSide(side1, side2) {
+//     let longestSide = (side1 + side2) - 1;
+  
+//     if (side1 > Math.abs(side2 - longestSide) && side2 > Math.abs(side1 - longestSide)) {
+//       return longestSide;
+//     } else {
+//       return "Keraksiz uchburchak. Ikki tomonning yig'indisi boshqa tomondan katta bo'lishi kerak";
+//     }
+//   }
+  
+//   let side1 = 10;
+//   let side2 = 8;
+//   let longestSideValue = findLongestSide(side1, side2);
+  
+//   if (typeof longestSideValue === 'string') {
+//     console.log(longestSideValue);
+//   } else {
+//     console.log(`Eng uzun uchburchak: ${longestSideValue}`);
+//   }
+  
+
+// 11-masala
+
+// function findRemaind(dividend, divisor) {
+//     return dividend % divisor;
+//   }
+  
+//   let remainder = findRemaind(3, 4);
+//   console.log(remainder);
